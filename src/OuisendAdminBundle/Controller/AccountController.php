d35f89717ae5337407e5ab8c5710424edcadc3ba
Adjete Tata DA SILVEIRA <dasilveira.tata@gmail.com>
2015-06-24 13:38:34 +0200

    model updating with Radmou at AH

--- src/OuisendAdminBundle/Controller/AccountController.php
+++ src/OuisendAdminBundle/Controller/AccountController.php
@@ -7,13 +7,68 @@ namespace OuisendAdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class HelloController extends Controller
+class AccountController extends Controller
 {
+	
+	public function getRepository () {
+		$repository = 
+    		$this->getDoctrine()
+            	 ->getRepository('OuisendAdminBundle:Account');
+		return $repository; 
+	}
+	
+	public function getManager () {
+		$manager = 
+    		$this->getDoctrine()
+            	 ->getManager ();
+		return $manager; 
+	}
+	
     public function indexAction()
     {
-    	// Version 1 du tuto
-        // return new Response("Ghost World !");
-        
-        return $this->render('OuisendAdminBundle:Hello:hello-world.html.twig', array('nom' => 'winzou'));
+    	// TODO complete fetching those records with parameters
+        $elements = getRepository()->findAll();
+        return $this->render('OuisendAdminBundle:Account:show.html.twig', array('data' => $elements));
     }
+	
+	public function detailAction($id) {
+		
+        $element = getRepository()->find($id);
+		
+		return $this->render('OuisendAdminBundle:Account:detail.html.twig', 
+							 array('data' => $element));
+	}
+	
+	public function deleteAction($id) {
+		
+        $element = getRepository()->find($id);
+		getRepository()->delete($element);
+		
+		return $this->redirect(
+			   $this->generateUrl('ouisendadmin_account_index'));
+	}
+	
+	public function createAction () {
+			
+		$element = new Account; 
+		$request = $this->get('request');
+		if ($request->getMethod() == 'POST') {
+
+			$form->bind($request);
+			if ($form->isValid()) {
+				getManager()->persist($element);
+				getManager()->flush();
+				
+				return $this->redirect(
+					   $this->generateUrl('ouisendadmin_account_detail', 
+					   					  array('data' => $element)));
+			}
+		}
+		
+		$form = $this->createForm(new AccountType, $element);
+		return $this->redirect(
+			   $this->generateUrl('ouisendadmin_account_detail', 
+			   					  array('data' => $element)));
+		
+	}
 }
--- src/OuisendAdminBundle/Controller/DefaultController.php
+++ src/OuisendAdminBundle/Controller/DefaultController.php
@@ -15,20 +15,49 @@ class DefaultController extends Controller
 	// The following will be for demo purposes 
 	public function ajouterAction() {
 		
+		// On récupère la requête
+		$request = $this->get('request');
+		// On vérifie qu'elle est de type POST
+		if ($request->getMethod() == 'POST') {
+			// On fait le lien Requête <-> Formulaire
+			// À partir de maintenant, la variable $article contient les valeurs entrées dans le formulaire par le visiteur
+			$form->bind($request);
+			
+			// On vérifie que les valeurs entrées sont correctes
+			// (Nous verrons la validation des objets en détail dans le prochain chapitre)
+			if ($form->isValid()) {
+				// On l'enregistre notre objet $article dans la base de données
+				$em = $this->getDoctrine()->getManager(); $em->persist($article);
+				$em->flush();
+				
+				// On redirige vers la page de visualisation de l'article nouvellement créé
+				return $this->redirect($this->generateUrl('sdzblog_voir', array('id' => $article->getId())));
+			}
+		}
+		// À ce stade :
+		// - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+		// - Soit la requête est de type POST, mais le formulaire n'est pas valide, donc on l'affiche de nouveau
+		
 		$account = new Account(); 
+		/*
 		// On crée le FormBuilder grâce à la méthode du contrôleur
-		$formBuilder = $this->createFormBuilder($article);
+		$formBuilder = $this->createFormBuilder($account);
 		// On ajoute les champs de l'entité que l'on veut à notre formulaire
 		$formBuilder
 		->add('date','date')  
 		->add('titre', 'text') 
 		->add('contenu', 'textarea') 
 		->add('auteur', 'text')
-		->add('publication', 'checkbox');
+		￼->add('publication', 'checkbox', array('required' => false))
+		;
 		
 		// Pour l'instant, pas de commentaires, catégories, etc., on les gérera plus tard
 		// À partir du formBuilder, on génère le formulaire
 		$form = $formBuilder->getForm();
+		*/
+		$form = $this->createForm(new AccountType, $account);
+		
+		
 		// On passe la méthode createView() du formulaire à la vue afin qu'elle puisse afficher le formulaire toute seule
 		return $this->render( 'OuisendAdminBundle:Default:ajouter.html.twig', 
 							  array ('form' => $form->createView(), ));
